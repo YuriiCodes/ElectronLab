daa79aabf6449cc4a73bb5cf9254922a8dce36fd
YuriiCodes <yuriipidlisnyi2021@gmail.com>
2022-11-11 14:00:44 +0200

    Added an example of valid XML input

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -11,16 +11,39 @@ const StartPage = () => {
       <h1 className="text-white">Lab</h1>
       <p className="text-white">Greetings! This app will help you convert .XML to .HTML file!</p>
       <p className="text-white">To start, just click the button below:</p>
-      <button type="button" className="btn btn-primary"><Link to="/loadFile"
-                                                              className="text-white text-decoration-none">Load
-        XML</Link></button>
+      <button type="button" className="btn btn-primary">
+        <Link to="/loadFile" className="text-white text-decoration-none">Load XML</Link>
+      </button>
+      <h3 className={"text-white mt-4"}>Example of valid XML input</h3>
+      <p>
+        <pre className={"text-white"}>
+          {`
+         <scientistpersonnel>
+          <group>
+            <name>Linear Algrebra</name>
+            <department>Mathematics</department>
+            <branch>Mathematical Analysis</branch>
+            <chair>Professor, Doctor of Technical Sciences, Professor of the Department of Computer Science and Engineering
+            </chair>
+            <day>Wednesday</day>
+            <time>18:00</time>
+            <headman>Alexander the great</headman>
+            <course>Linear algebra basics</course>
+            <subject>Computer Science</subject>
+            <leader>Kozlovsky Alexander</leader>
+          </group>
+        </scientistpersonnel>
+          `}
+        </pre>
+
+      </p>
     </div>
   );
 };
 
 export default function App() {
   const [json, setJson] = useState({})
-const [isJsonLoaded, setIsJsonLoaded] = useState(false)
+  const [isJsonLoaded, setIsJsonLoaded] = useState(false)
   // Get parsed JSON from the main process
   window.electron.ipcRenderer.on('json-ready', (args) => {
     // @ts-ignore
